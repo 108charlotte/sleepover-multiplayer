3dfe6ce8d0a15952428c666acd65af908df00879
solace <143859954+solacite@users.noreply.github.com>
2026-04-26 08:04:49 -0500

    finalize cover; smooth anim

--- demo/camera_2d.gd
+++ demo/camera_2d.gd
@@ -4,7 +4,7 @@ func _process(delta):
 	var mouse = get_global_mouse_position()
 	var center = get_viewport_rect().size / 2
 	var target = Vector2(
-		clamp(mouse.x, center.x - 100, center.x + 100),
-		clamp(mouse.y, center.y - 100, center.y + 100)
+		clamp(mouse.x, center.x - 20, center.x + 20),
+		clamp(mouse.y, center.y - 20, center.y + 20)
 	)
 	position = lerp(position, target, 0.05)
